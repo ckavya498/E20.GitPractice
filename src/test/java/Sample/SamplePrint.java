9d9c7538b1aea7b6fc39242f14a7ed7fb3c37625
kavya <kavya@LAPTOP-SR1F18AB>
2025-05-15 16:28:32 +0530

    Demo class committed

---
+++ src/test/java/Sample/SamplePrint.java
@@ -0,0 +1,16 @@
+package Sample;
+
+public class SamplePrint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 50;
+		int c = a+b;
+		System.out.println("Adding the numbers");
+		System.out.println("Value is" +  c);
+	    System.out.println("Hello");
+	}
+
+}
