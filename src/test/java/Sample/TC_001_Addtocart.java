b6e997af7de9428f82690ac887958cb27c031513
kavya <kavya@LAPTOP-SR1F18AB>
2025-05-16 17:53:46 +0530

    Test script committed

---
+++ src/test/java/Sample/TC_001_Addtocart.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class TC_001_Addtocart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Add to Cart");
+	}
+
+}
